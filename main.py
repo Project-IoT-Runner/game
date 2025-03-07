d1731d1b21887127dae1b547322baf9dc9f69910
Pixel5908 <ruben.thie@hotmail.com>
2025-03-07 11:00:00 +0100

    player can check for button input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from machine import Pin
 import utime
-button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+button_up = Pin(14, Pin.IN, Pin.PULL_DOWN)
+button_down = Pin(15, Pin.IN, Pin.PULL_DOWN)
 led = Pin('LED', Pin.OUT)
 
 class Player():
@@ -8,10 +9,10 @@ class Player():
         pass
     
     def move_up(self):
-        pass
+        print('Up')
     
     def move_down(self):
-        pass
+        print('Down')
     
     def get_collition(self) -> bool:
         pass
@@ -19,9 +20,14 @@ class Player():
     def kill_player(self):
         pass
     
+    def check_player_input(self, up, down):
+        if up.value() == 1:
+            self.move_up()
+            utime.sleep(0.05)
+        elif down.value() == 1:
+            self.move_down()
+            utime.sleep(0.05)
+
+player = Player()
 while True:
-    if button.value() == 1:
-        led.value(1)
-        utime.sleep(.2)
-    else:
-        led.value(0)
+    player.check_player_input(button_up, button_down)
